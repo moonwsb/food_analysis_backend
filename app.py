ea20e88ea1681625d4cf0737078b0302b06db432
Esra <esra@Esra-MacBook-Air.local>
2026-06-10 15:44:54 +0300

    Load models

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import joblib
 
 app = FastAPI()
+
+model = joblib.load("colyak_model.pkl")
+diabetes_model = joblib.load("diyabet_model.pkl")
 
 @app.get("/")
 def home():
